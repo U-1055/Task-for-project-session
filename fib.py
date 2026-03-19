5754aa09620625ec4a06dbccb1e9a432a266dd2f
U-1055 <filatov_truba@mail.ru>
2026-03-19 18:48:45 +0700

    .

--- fib.py
+++ fib.py
@@ -17,10 +17,10 @@ def gen_fib(start_: int, end_: int) -> list[int]:
 
 
 NO_NUMS = 'В заданном диапазоне нет чисел Фибоначчи'
-start, end = map(int, input().split())
-
-if start < end:
-    res = gen_fib(start, end)
-    print(' '.join(map(str, res)) if res else NO_NUMS)
-else:
-    print(NO_NUMS)
+while True:
+    start, end = map(int, input().split())
+    if start < end:
+        res = gen_fib(start, end)
+        print(' '.join(map(str, res)) if res else NO_NUMS)
+    else:
+        print(NO_NUMS)
